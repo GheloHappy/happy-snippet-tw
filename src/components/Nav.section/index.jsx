24ebe5509594a420ba2893804ae3578766823f21
Ghelonico <ghelonicomaligaya111@gmail.com>
2024-03-01 16:54:33 +0800

    update: nav

--- src/components/Nav.section/index.jsx
+++ src/components/Nav.section/index.jsx
@@ -2,9 +2,9 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className="sticky w-full bg-gray-700 p-3 justify-between items-center flex text-white">
-            <Link to="/" className="text-[2rem]">Happy Snippets</Link>
-            <ul className="flex gap-10 justify-end">
+        <nav className="sticky w-full bg-gray-700 p-3 md:justify-between md:items-center flex flex-col md:flex-row text-white shadow">
+            <Link to="/" className="text-[1.5rem] md:text-[2rem] ml-5 md:ml-2">Happy Snippets</Link>
+            <ul className="flex-col gap-5 md:gap-10 md:flex-row flex justify-start pt-5 md:pt-0 ml-5 w-full h-full ">
                 <CustomLink to="/home">Home</CustomLink>
                 <CustomLink to="/snippets">Snippets</CustomLink>
                 <CustomLink to="/profile">Profile</CustomLink>
@@ -18,7 +18,7 @@ function CustomLink({ to, children, ...props }) {
     const isActive = useMatch({path: resolvedPath.pathname, end:true})
 
     return (
-        <li className={`text-[1.2rem] ${isActive ? "text-red-400" : ""}`}>
+        <li className={`text-[1.2rem] font-semibold ${isActive ? "text-red-400" : ""}`}>
             <Link to={to} {...props}>
                 {children}
             </Link>
