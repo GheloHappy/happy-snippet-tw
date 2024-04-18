03f9b74a516488ebfe78a62098e629167b4e848c
Ghelonico <ghelonicomaligaya111@gmail.com>
2024-04-18 11:27:16 +0800

    update home

--- src/components/Home.section/CodeSnippetCard.section/index.jsx
+++ src/components/Home.section/CodeSnippetCard.section/index.jsx
@@ -1,14 +1,17 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
-const CodeSnippetCard = ({language, theme, snippet}) => {
+const CodeSnippetCard = ({ language, theme, snippet }) => {
     return (
         <div className='text-start w-full h-screen'>
-            <label className="font-semibold">Preview: </label>
-            <div className="w-full">
-                <SyntaxHighlighter language={language} style={theme} showLineNumbers={true}>
-                    {snippet}
-                </SyntaxHighlighter>
+            <div className='flex flex-col items-center justify-center'>
+                <div className="w-3/4">
+                    <label className="font-semibold">Preview: </label>
+                    <SyntaxHighlighter language={language} style={theme} showLineNumbers={true}>
+                        {snippet}
+                    </SyntaxHighlighter>
+                </div>
             </div>
+
         </div>
     )
 }
