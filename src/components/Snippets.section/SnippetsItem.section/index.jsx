3fb63597ba1a085111d391d0577ffabdf823f6fb
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-05-01 00:17:10 +0800

    update: snippet items data map

--- src/components/Snippets.section/SnippetsItem.section/index.jsx
+++ src/components/Snippets.section/SnippetsItem.section/index.jsx
@@ -3,12 +3,14 @@ const SnippetsItem = ({ data }) => {
     return (
         <>
             {data.length > 0 ? (
-                data.map((item, index) => {
-                    <div key={index} className="w-full bg-white rounded flex">
-                        <h1>{item.snippet_title}</h1>
+                data.map((item, index) => (
+                    <div key={index} className="w-full bg-white rounded flex text-[1rem]">
+                        <h1 className="">{item.id}</h1>
                     </div>
-                })
-            ) : <h1 className="text-white">No data</h1>}
+                ))
+            ) : (
+                <h1 className="text-white">No data</h1>
+            )}
         </>
     )
 }
