d6567245bf97b153a6e51d69bc751c90be4fef1c
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-05-01 21:31:51 +0800

    update: snippets ui

--- src/components/Snippets.section/SnippetsItem.section/index.jsx
+++ src/components/Snippets.section/SnippetsItem.section/index.jsx
@@ -1,13 +1,15 @@
 const SnippetsItem = ({ data }) => {
-    
+
     return (
         <div className="md:w-3/4 w-full">
             {data.length > 0 ? (
                 data.map((item, index) => (
-                    <div key={index} className="w-full bg-white rounded flex text-[1rem] p-3 mt-3 gap-5">
-                        <h1 className="">{item.date}</h1>
-                        <h1 className="">{item.snippet_title}</h1>
-                        <h1 className="">{item.snippet_language}</h1>
+                    <div key={index} className="w-full bg-white rounded flex text-[1rem] p-3 mt-3 gap-5 overflow-auto ">
+                        <div className="grid grid-cols-3 w-full">
+                            <h1 className="">{item.date}</h1>
+                            <h1 className="">{item.snippet_title}</h1>
+                            <h1 className="">{item.snippet_language}</h1>
+                        </div>
                     </div>
                 ))
             ) : (
--- src/components/Snippets.section/index.jsx
+++ src/components/Snippets.section/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import SnippetsItem from "./SnippetsItem.section"
 import { getData } from "../../utils/fetcher"
 import { useSelector } from "react-redux"
+import { IoIosSearch } from "react-icons/io"
 
 const SnippetSection = () => {
     const user_id = useSelector((state) => state.user.user_id)
@@ -25,7 +26,13 @@ const SnippetSection = () => {
     }, [user_id])
 
     return (
-        <div className="flex-col flex mt-[3.5rem] md:mt-[4.5rem] w-full p-5 gap-1 md:gap-3 items-center justify-center">
+        <div className="flex-col flex mt-[2.5rem] md:mt-[4.5rem] w-full p-5 md:gap-3 items-center justify-center">
+            <div className="w-full bg-white rounded p-1 flex gap-3">
+                <input className="text-[1.1rem] w-full border rounded p-2" 
+                 placeholder={`Search Title`}
+                type="text" />
+                <button className="text-[2rem]"><IoIosSearch /> </button>
+            </div>
             <SnippetsItem data={data} />
         </div>
     )
