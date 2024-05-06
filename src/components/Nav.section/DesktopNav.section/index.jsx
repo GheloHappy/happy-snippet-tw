7ef3ea81924d6c0458d7756a5c2b351848277a40
Ghelonico <ghelonicomaligaya111@gmail.com>
2024-05-06 16:49:07 +0800

    update: Nav design

--- src/components/Nav.section/DesktopNav.section/index.jsx
+++ src/components/Nav.section/DesktopNav.section/index.jsx
@@ -1,23 +1,65 @@
-import { MdOutlineMenu } from "react-icons/md";
+import { MdOutlineMenu, MdOutlineSettings } from "react-icons/md";
 import { useDispatch } from "react-redux";
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link, useMatch, useNavigate, useResolvedPath } from "react-router-dom";
 import { setNavState } from "../../../redux/system.redux.j/systemActions";
+import { GiDiamondsSmile } from "react-icons/gi";
+import { useState } from "react";
+import { RiShutDownLine } from "react-icons/ri";
+import { FaExclamation } from "react-icons/fa";
+
 
 const DesktopNav = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isProfile, setIsProfile] = useState(false)
+
+    const handleProfile = () => {
+        setIsProfile(!isProfile);
+    }
+
+    const handleLogout = () => {
+        localStorage.clear();
+        cookies.remove('_hs');
+        dispatch(setNavState(false))
+        navigate('/');
+    }
 
     return (
         <nav className="fixed top-0 w-full bg-gray-700 p-3 md:justify-between md:items-center flex md:flex-row text-white shadow">
             <Link to="/" className="text-[1.2rem] sm:text-[1.5rem] md:text-[2rem] md:ml-2 w-full items-center justify-center">Happy Snippets</Link>
-            <ul className="hidden md:flex justify-end gap-10">
+            <ul className="hidden md:flex justify-end gap-10 md:items-center md:justify-center">
                 <CustomLink to="/home">Home</CustomLink>
                 <CustomLink to="/snippets">Snippets</CustomLink>
                 {/* <CustomLink to="/settings">Settings</CustomLink> */}
-                <CustomLink to="/profile">Profile</CustomLink>
+                <div className="w-full text-[2rem] bg-black rounded-full p-2"
+                    onClick={handleProfile}>
+                    <GiDiamondsSmile />
+                    {
+                        isProfile ?
+                            <div className="w-[15%] flex flex-col absolute bg-white text-black text-[1.1rem] font-bold right-4 top-16 rounded  p-3 gap-3">
+                                <span className="w-full flex gap-2 items-center justify-center">
+                                    <MdOutlineSettings />
+                                    <Link to="/settings">Settings</Link>
+                                </span>
+                                <span className="w-full flex gap-2 items-center justify-center">
+                                    <FaExclamation />
+                                    <Link to="/about">About</Link>
+                                </span>
+                                <span className="w-full flex flex-col items-center justify-center p-3  border rounded bg-slate-50">
+                                    <span className="flex items-center justify-center gap-2 text-red-500"
+                                    onClick={handleLogout}>
+                                        <RiShutDownLine />
+                                        Logout
+                                    </span>
+                                </span>
+                            </div>
+                            : null
+                    }
+                </div>
             </ul>
             <div className="md:hidden flex items-center justify-end w-full ">
-                <button  onClick={() => {dispatch(setNavState(true))}}
-                className="text-[2rem]"><MdOutlineMenu /></button>
+                <button onClick={() => { dispatch(setNavState(true)) }}
+                    className="text-[2rem]"><MdOutlineMenu /></button>
             </div>
         </nav>
     )
@@ -29,7 +71,7 @@ export function CustomLink({ to, children, ...props }) {
     const dispatch = useDispatch();
 
     return (
-        <li className={`text-[1.2rem] font-semibold ${isActive ? "text-red-400" : ""}`} onClick={() => {dispatch(setNavState(false))}}>
+        <li className={`md:text-[1.2rem] text-[1.5rem] font-semibold ${isActive ? "text-red-400" : ""}`} onClick={() => { dispatch(setNavState(false)) }}>
             <Link to={to} {...props} >
                 {children}
             </Link>
--- src/components/Nav.section/MobileNav.section/index.jsx
+++ src/components/Nav.section/MobileNav.section/index.jsx
@@ -4,6 +4,10 @@ import { setNavState } from "../../../redux/system.redux.j/systemActions"
 import { CustomLink } from "../DesktopNav.section"
 import { Link, useNavigate } from "react-router-dom"
 import cookies from "react-cookies"
+import { GiDiamondsSmile } from "react-icons/gi"
+import { RiShutDownLine } from "react-icons/ri"
+import { FaExclamation } from "react-icons/fa"
+import { MdOutlineKeyboardArrowRight } from "react-icons/md"
 
 const MobileNav = () => {
     const dispatch = useDispatch()
@@ -20,27 +24,43 @@ const MobileNav = () => {
         <div className="fixed flex flex-col justify-end items-end top-0 w-full h-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm md:hidden">
             <div className="h-full w-4/5 bg-white flex flex-col ">
                 <div className="flex w-full p-3 border items-center justify-center">
-                    <div className="w-full">
+                    {/* <div className="w-full">
                         <h1 className="text-[1.5rem] ">Snippet</h1>
-                    </div>
+                    </div> */}
                     <div className="w-full h-full text-end flex flex-col items-end justify-center">
-                        <button onClick={() => { dispatch(setNavState(false)) }} className="text-[1.5rem] "><RxCross1 /></button>
+                        <button onClick={() => { dispatch(setNavState(false)) }} className="text-[2rem] "><RxCross1 /></button>
                     </div>
                 </div>
-                <div className="w-full bg-red-100 flex flex-col items-start justify-center h-full">
-                    <ul className="flex flex-col justify-end gap-10">
+                <div className="w-full flex flex-col items-start justify-center h-full">
+                    <ul className="flex flex-col justify-end gap-10 pl-4">
                         <CustomLink to="/home">Home</CustomLink>
                         <CustomLink to="/snippets">Snippets</CustomLink>
                         <CustomLink to="/settings">Settings</CustomLink>
                     </ul>
                 </div>
-                <div className="w-full bg-slate-100 h-1/6 items-center justify-center flex flex-col">
-                    <Link to="/profile">Profile</Link>
+                <div className="w-full border-t h-[10%] items-start px-3 p-4">
+                    <div className="p-2 bg-black text-white rounded-full text-[1.5rem] sm:text-[2rem] gap-4 flex items-center justify-center">
+                        <GiDiamondsSmile />
+                        <span className="font-flower">Snippet User</span>
+                    </div>
+                   
                 </div>
-                <div className="w-full bg-green-200">
-                    <h1>About</h1>
+                <div className="w-full flex text-center text-[1.2rem] font-bold gap-2 p-4">
+                    <div className="flex items-center gap-2 w-1/2">
+                        <FaExclamation />
+                        <Link to="/about">About</Link>
+                    </div>
+                    <div className="flex text-[1.5rem] font-bold w-1/2 justify-end">
+                        <MdOutlineKeyboardArrowRight />
+                    </div>
                 </div>
-                <button onClick={handleLogout}>Logout</button>
+                <div className="w-full flex flex-col items-center justify-center text-[1.3rem] p-3 border rounded bg-slate-50"
+                    onClick={handleLogout}>
+                    <button
+                        className="flex items-center text justify-center gap-2 font-bold text-red-500 ">
+                        <RiShutDownLine />
+                        Logout</button>
+                </div>
             </div>
         </div>
 
