59a2e082b2525b2be1b0995014efd302a158f744
Ghelonico <ghelonicomaligaya111@gmail.com>
2024-06-06 14:55:21 +0800

    edit snippets update

--- src/components/Snippets.section/EditSnippet.section/index.jsx
+++ src/components/Snippets.section/EditSnippet.section/index.jsx
@@ -4,19 +4,44 @@ import languageOptions from '../../Home.section/CodeSnippet.section/Language.opt
 import { RxCross1 } from 'react-icons/rx';
 import { setSnippet, setSnippetEditing, setSnippetLanguage } from '../../../redux/snippet.redux/snippetActions';
 import { GrUpdate } from 'react-icons/gr';
+import { useState, useEffect } from 'react';
 
 const EditSnippet = () => {
-    const dispatch = useDispatch()
-    const snippet = useSelector((state) => state.snippet.snippet_code)
-    const language = useSelector((state) => state.snippet.snippet_language)
-    const snippet_title = useSelector((state) => state.snippet.snippet_title)
-    const is_public = useSelector((state) => state.snippet.snippet_privacy)
+    const dispatch = useDispatch();
+    const snippet = useSelector((state) => state.snippet.snippet_code);
+    const language = useSelector((state) => state.snippet.snippet_language);
+    const snippet_title = useSelector((state) => state.snippet.snippet_title);
+    const is_public = useSelector((state) => state.snippet.snippet_privacy);
+
+    const [fields, setFields] = useState({
+        language: '',
+        snippet_title: '',
+        is_public: false,
+        snippet: ''
+    });
+
+    useEffect(() => {
+        setFields({
+            language: language,
+            snippet_title: snippet_title,
+            is_public: is_public,
+            snippet: snippet,
+        });
+    }, [language, snippet, snippet_title, is_public]);
 
     const handleCloseClear = () => {
-        dispatch(setSnippetEditing(false))
-        dispatch(setSnippet(''))
-        dispatch(setSnippetLanguage('javascript'))
-    }
+        dispatch(setSnippetEditing(false));
+        dispatch(setSnippet(''));
+        dispatch(setSnippetLanguage('javascript'));
+    };
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFields(prevFields => ({
+            ...prevFields,
+            [name]: type === 'checkbox' ? checked : value,
+        }));
+    };
 
     return (
         <div className="w-full h-screen absolute top-0 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm p-2 md:p-10 rounded">
@@ -33,7 +58,9 @@ const EditSnippet = () => {
                         <label className="font-semibold text-[1.2rem]">Language: </label>
                         <select
                             className="w-5/6 p-2 rounded text-black"
-                            defaultValue={language}
+                            name="language"
+                            value={fields.language}
+                            onChange={handleChange}
                         >
                             {languageOptions.map(option => (
                                 <option key={option.value} value={option.value}>{option.label}</option>
@@ -42,8 +69,13 @@ const EditSnippet = () => {
                     </div>
                     <div className='w-full flex md:items-center p-0 md:p-2 md:flex-row flex-col items-start gap-2'>
                         <label className='font-semibold text-[1.2rem]'>Title: </label>
-                        <input type='text' className='w-full p-2 text-black font-normal rounded' 
-                        defaultValue={snippet_title}/>
+                        <input 
+                            type='text' 
+                            className='w-full p-2 text-black font-normal rounded' 
+                            name="snippet_title"
+                            value={fields.snippet_title}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className='w-full flex items-center justify-start md:justify-end md:pr-2'>
                         <label className='font-semibold cursor-pointer'>
@@ -51,8 +83,9 @@ const EditSnippet = () => {
                                 type="checkbox"
                                 className="mr-2"
                                 name='is_public'
-                                defaultChecked={is_public}
-                            /> Make it {is_public ? "private" : "public" } ?
+                                checked={fields.is_public}
+                                onChange={handleChange}
+                            /> Make it {fields.is_public ? "private" : "public"} ?
                         </label>
                     </div>
                 </div>
@@ -61,25 +94,19 @@ const EditSnippet = () => {
                     minRows={10}
                     maxRows={30}
                     placeholder="Enter your snippet here..."
-                    value={snippet}
+                    name="snippet"
+                    value={fields.snippet}
+                    onChange={handleChange}
                 />
                 <div className='w-full flex items-center justify-center gap-5 px-2'>
-                    {/* <div className='w-1/2 flex flex-col items-start'>
-                        <button className='border-white border-[2px] p-2 text-[1.5rem] rounded flex items-center'>
-                            <GrFormView />
-                            View
-                        </button>
-                    </div> */}
-                    {/* <div className='w-1/2 flex flex-col items-end'> */}
                     <button className='w-full justify-center border-white border-[2px] p-2 text-[1.2rem] rounded flex items-center gap-2'>
                         <GrUpdate />
                         Update
                     </button>
-                    {/* </div> */}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default EditSnippet
+export default EditSnippet;
