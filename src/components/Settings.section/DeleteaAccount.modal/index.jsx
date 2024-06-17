a86104eb7b011c28c9b235a0a27f96a110660177
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-06-17 16:47:28 +0800

    update: Delete account ui

---
+++ src/components/Settings.section/DeleteaAccount.modal/index.jsx
@@ -0,0 +1,22 @@
+const DeleteAccount = ({setIsDelete}) => {
+
+    return (
+        <div className="w-full h-screen absolute top-0 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm p-2 md:p-10 rounded flex flex-col items-center justify-center">
+            <div className="bg-gray-100 mx-3 md:mx-0 md:w-1/2 rounded p-5">
+                <h1 className="text-[1.5rem] font-bold text-center">Are you sure you want to delete your account ?</h1>
+                <div className="w-full flex items-center justify-center p-1 my-2 gap-5">
+                    <label className="font-semibold text-[1.2rem]">Input your password: </label>
+                    <input type="password" className="p-2 w-1/2 rounded text-[1.2rem]" placeholder="Your Password"/>
+                </div>
+                <div className="w-full flex items-center justify-center gap-3 text-white text-[1.5rem] mt-5">
+                    <button className="w-full bg-blue-500 rounded p-2 font-semibold"
+                    onClick={() => { setIsDelete(false) }}>Cancel</button>
+                    <button className="w-full bg-red-500 rounded p-2 font-semibold"
+                    >Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DeleteAccount
--- src/pages/Settings.page/index.jsx
+++ src/pages/Settings.page/index.jsx
@@ -2,41 +2,46 @@ import { useState } from 'react';
 import Information from '../../components/Settings.section/Information.section'
 import ThemeSettings from '../../components/Settings.section/ThemeSettings.section'
 import ChangeAccountSettings from '../../components/Settings.section/ChangeAccountSettings.section'
+import DeleteAccount from '../../components/Settings.section/DeleteaAccount.modal';
 
 const Settings = () => {
     const [settingsNum, setSettingsNum] = useState(1)
+    const [isDelete, setIsDelete] = useState(false)
 
     return (
-        <div className="w-full h-screen text-white ">
-            <div className='w-full h-full md:flex pt-10 mt-[1.5rem] md:mt-[2.5rem] p-1'>
-                <div className='md:w-[30%] flex md:flex-col items-center md:pt-10 border-white border md:rounded-l-lg'>
-                    <div className='md:gap-2 flex md:flex-col font-bold md:text-[1.5rem] text-start w-full'>
-                        <button className={`w-full p-5 ${settingsNum === 1 ? 'bg-gray-50 text-black' : "bg-none"}`} onClick={() => setSettingsNum(1)}>
-                            <span className="hidden sm:inline">Information</span>
-                            <span className="sm:hidden">Info</span>
-                        </button>
-                        <button className={`w-full p-5 ${settingsNum === 2 ? 'bg-white text-black' : "bg-none"}`} onClick={() => setSettingsNum(2)}>Theme Settings</button>
-                        <button className={`w-full p-5 ${settingsNum === 3 ? 'bg-white text-black' : "bg-none"}`} onClick={() => setSettingsNum(3)}>Account Settings</button>
+        <>
+            {isDelete ? <DeleteAccount setIsDelete={setIsDelete}/> : null}
+            <div className="w-full h-screen text-white ">
+                <div className='w-full h-full md:flex pt-10 mt-[1.5rem] md:mt-[2.5rem] p-1'>
+                    <div className='md:w-[30%] flex md:flex-col items-center md:pt-10 border-white border md:rounded-l-lg'>
+                        <div className='md:gap-2 flex md:flex-col font-bold md:text-[1.5rem] text-start w-full'>
+                            <button className={`w-full p-5 ${settingsNum === 1 ? 'bg-gray-50 text-black' : "bg-none"}`} onClick={() => setSettingsNum(1)}>
+                                <span className="hidden sm:inline">Information</span>
+                                <span className="sm:hidden">Info</span>
+                            </button>
+                            <button className={`w-full p-5 ${settingsNum === 2 ? 'bg-white text-black' : "bg-none"}`} onClick={() => setSettingsNum(2)}>Theme Settings</button>
+                            <button className={`w-full p-5 ${settingsNum === 3 ? 'bg-white text-black' : "bg-none"}`} onClick={() => setSettingsNum(3)}>Account Settings</button>
+                        </div>
+                    </div>
+                    <div className='md:w-[70%] h-full flex flex-col items-center justify-center bg-gray-50 text-black md:rounded-r-lg p-3'>
+                        {
+                            (() => {
+                                switch (settingsNum) {
+                                    case 1:
+                                        return <Information />
+                                    case 2:
+                                        return <ThemeSettings />
+                                    case 3:
+                                        return <ChangeAccountSettings setIsDelete={setIsDelete}/>
+                                    default:
+                                        return <Information />
+                                }
+                            })()
+                        }
                     </div>
                 </div>
-                <div className='md:w-[70%] h-full flex flex-col items-center justify-center bg-gray-50 text-black md:rounded-r-lg p-3'>
-                    {
-                        (() => {
-                            switch (settingsNum) {
-                                case 1:
-                                    return <Information />
-                                case 2:
-                                    return <ThemeSettings />
-                                case 3:
-                                    return <ChangeAccountSettings />
-                                default:
-                                    return <Information />
-                            }
-                        })()
-                    }
-                </div>
             </div>
-        </div>
+        </>
     )
 }
 
