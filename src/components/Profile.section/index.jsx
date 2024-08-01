a625533d93d0bace6a8f8c8bd30c05e6cfe9a8f8
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-08-01 23:42:55 +0800

    update: profile layout

--- src/components/Profile.section/index.jsx
+++ src/components/Profile.section/index.jsx
@@ -1,7 +1,19 @@
 const ProfileSection = () => {
+    const info = JSON.parse(localStorage.getItem('user_info'))
+
     return (
         <div className="flex-col flex mt-[2.5rem] md:mt-[4.5rem] w-full pt-6 md:gap-3 items-center justify-center text-white">
-            <h1 className="text-white">Profile</h1>
+            <div className="w-[70%] bg-red-50">
+                <div className="w-full flex text-black">
+                    <div className="w-[30%] flex justify-center flex-col items-center">
+                        <h1>IMAGE</h1>
+                        <h1>{info ? info.display_name : "Snippet User"}</h1>
+                    </div>
+                    <div className="w-[70%] flex justify-center">
+                        <h1>STATS</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
