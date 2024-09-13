a76501ffc4f8a6307f505ef7be63a731b2201eec
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-09-13 23:05:55 +0800

    update: delete/remove modal change from absolute to fixed

--- src/components/Settings.section/DeleteaAccount.modal/index.jsx
+++ src/components/Settings.section/DeleteaAccount.modal/index.jsx
@@ -1,7 +1,7 @@
 const DeleteAccount = ({setIsDelete}) => {
 
     return (
-        <div className="w-full h-screen absolute top-0 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm p-2 md:p-10 rounded flex flex-col items-center justify-center">
+        <div className="w-full h-screen fixed top-0 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm p-2 md:p-10 rounded flex flex-col items-center justify-center">
             <div className="bg-gray-100 mx-3 md:mx-0 md:w-1/2 rounded p-5">
                 <h1 className="text-[1.5rem] font-bold text-center">Are you sure you want to delete your account ?</h1>
                 <div className="w-full flex items-center justify-center p-1 my-2 gap-5">
--- src/components/Snippets.section/RemoveApproval.modal/index.jsx
+++ src/components/Snippets.section/RemoveApproval.modal/index.jsx
@@ -1,7 +1,7 @@
 const RemoveApproval = ({ setIsRemoving, onDelete, title }) => {
     return (
 
-        <div className="w-full h-screen absolute flex flex-col overflow-auto items-center justify-center bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm">
+        <div className="w-full h-screen fixed top-0 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm p-2 md:p-10 rounded flex flex-col items-center justify-center">
             <div className="bg-gray-100 mx-3 md:mx-0 md:w-1/2 rounded p-5">
                 <h1 className="text-[1.5rem] font-bold text-center">Are you sure you want to delete snippet
                     "<span className="text-red-600">{title}</span>" ?</h1>
