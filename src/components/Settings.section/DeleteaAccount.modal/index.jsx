05e5893681d3de9c5b255456c1f92884441ece7f
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-09-14 23:08:57 +0800

    update: delete account responsive

--- src/components/Settings.section/DeleteaAccount.modal/index.jsx
+++ src/components/Settings.section/DeleteaAccount.modal/index.jsx
@@ -4,9 +4,9 @@ const DeleteAccount = ({setIsDelete}) => {
         <div className="w-full h-screen fixed top-0 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm p-2 md:p-10 rounded flex flex-col items-center justify-center">
             <div className="bg-gray-100 mx-3 md:mx-0 md:w-1/2 rounded p-5">
                 <h1 className="text-[1.5rem] font-bold text-center">Are you sure you want to delete your account ?</h1>
-                <div className="w-full flex items-center justify-center p-1 my-2 gap-5">
+                <div className="w-full flex-col md:flex-row flex items-center justify-center p-1 my-2 gap-5 mt-4">
                     <label className="font-semibold text-[1.2rem]">Input your password: </label>
-                    <input type="password" className="p-2 w-1/2 rounded text-[1.2rem]" placeholder="Your Password"/>
+                    <input type="password" className="p-2 w-full md:w-1/2 rounded text-[1.2rem]" placeholder="Your Password"/>
                 </div>
                 <div className="w-full flex items-center justify-center gap-3 text-white text-[1.5rem] mt-5">
                     <button className="w-full bg-blue-500 rounded p-2 font-semibold"
